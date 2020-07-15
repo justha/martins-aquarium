de6e3cb766a4767bb372bd60dff6b434714927bb
justha <nganha42@gmail.com>
2020-07-15 11:51:12 -0500

    added fishData.js

---
+++ scripts/fishData.js
@@ -0,0 +1,26 @@
+const fishCollection = [
+    {   
+        Name: "Chartreuse (Char)",
+        Species: "yellow tang",
+        Length in Inches: 8
+        Location: "Hawaiian Islands",
+        Food: "benthic turf algae", 
+        Image: "./images/fish_yellow_tang.jpg"
+    }, 
+    {
+        Name: "Paul Jr. (PJ)", 
+        Species: "pajama cardinalfish", 
+        Length in Inches: 3
+        Location: "Western Pacific Ocean (Java, Fiji, Ryukyu Islands, Great Barrier Reef",
+        Food: "small crustaceans, mysis shrimp, brine shrimp", 
+        Image: "./images/fish_pajama_cardinalfish.jpg"
+    }, 
+    {
+        Name: "Dorothy (Dotty)",
+        Species: "royal dottyback", 
+        Length in Inches: 3
+        Location: "Indo-Pacific Ocean"
+        Food: "brine shrimp, krill that contain carotenoids", 
+        Image: "./images/fish_royal_dottyback.jpg"
+    }
+]
