4d7fcdee99c3da74ebcc363eae8734c9475f2775
justha <nganha42@gmail.com>
2020-07-16 10:51:02 -0500

    fixed tip module feed

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
 
 import {fishList} from "./fishList.js"
-
 fishList()
 
 
 
 import {quoteList} from "./quoteList.js"
-
 quoteList()
 
 
 import {locationList} from "./locationList.js"
-
 locationList()
+
+import {tipList} from "./tipList.js"
+tipList()
